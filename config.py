988c05113fe9382109ae672a2bda4dc1f82f6711
Sean Breckenridge <seanbrecke@gmail.com>
2020-06-19 15:24:32 -0700

    finish update script, push README

--- config.py
+++ config.py
@@ -5,6 +5,16 @@ import os
 this_dir = os.path.abspath(os.path.dirname(__file__))
 
 github_username = "seanbreckenridge"
-ignore_repos = set(["st" "notes"])
+ignore_repos = set(["st",
+                    "notes",
+                    "advent-of-code-2019",
+                    "rsa",
+                    "projects",
+                    "cs-assignments",
+                    "plus1",
+                    "commands-discordpy-starter",
+                    "jikanpy_ext",
+                    "greasyfork",
+                    ])
 include_forks = False
 data_dir = os.path.join(this_dir, "data")
