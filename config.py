a4cce5c55547d90ab0f2399c8e7a7abc7b9cf9ec
Sean Breckenridge <seanbrecke@gmail.com>
2020-06-20 17:06:39 -0700

    auto-format python and use regex strings

--- config.py
+++ config.py
@@ -5,16 +5,19 @@ import os
 this_dir = os.path.abspath(os.path.dirname(__file__))
 
 github_username = "seanbreckenridge"
-ignore_repos = set(["st",
-                    "notes",
-                    "advent-of-code-2019",
-                    "rsa",
-                    "projects",
-                    "cs-assignments",
-                    "plus1",
-                    "commands-discordpy-starter",
-                    "jikanpy_ext",
-                    "greasyfork",
-                    ])
+ignore_repos = set(
+    [
+        "st",
+        "notes",
+        "advent-of-code-2019",
+        "rsa",
+        "projects",
+        "cs-assignments",
+        "plus1",
+        "commands-discordpy-starter",
+        "jikanpy_ext",
+        "greasyfork",
+    ]
+)
 include_forks = False
 data_dir = os.path.join(this_dir, "data")
