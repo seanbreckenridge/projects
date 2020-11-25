9dba1ad7996fb5187fa0d55134170cf218896da0
Sean Breckenridge <seanbrecke@gmail.com>
2020-11-25 02:40:28 -0800

    use next/image, resolves #2

--- components/lazy_image.tsx
+++ components/lazy_image.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 import {Dimensions} from "../lib/parseData";
 
@@ -8,15 +10,31 @@ interface ILazyImage {
   dimensions?: Dimensions;
 }
 
+const maxWidth = 400;
+const maxHeight = 400;
+
 const LazyImage = React.memo(({src, name, dimensions}: ILazyImage) => {
-  console.log(src)
+
+  const ratio = Math.min(maxWidth / dimensions.width, maxHeight / dimensions.height);
+
+  const useWidth = dimensions.width * ratio;
+  const useHeight = dimensions.height * ratio;
+
   return (
     <>
       {src != null
         ? (
-          <a href={src} target="_blank">
-            <img src={src} alt={`${name}`} />
-          </a>
+          <div className={styles.lazyImageContainer}>
+            <a href={src} target="_blank">
+              <Image
+                src={src}
+                alt={`${name}`}
+                width={useWidth}
+                height={useHeight}
+                loading="lazy"
+              />
+            </a>
+          </div>
         ) : (
           <></>
         )
