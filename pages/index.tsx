677e1ba40ef5c96ebf934c674d26878d799880be
Sean Breckenridge <seanbrecke@gmail.com>
2022-02-11 21:13:09 -0800

    add back arrow

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Script from "next/script";
 
 import styles from "../styles/Home.module.css";
 
@@ -28,12 +29,16 @@ export default function Home({ repos }: IHome) {
           name="description"
           content="A list of Sean Breckenridge's Projects"
         />
+        <meta property="ba:title" content="back home" />
+        <meta property="ba:url" content="https://sean.fish" />
+        <meta property="ba:color" content="rgb(156, 181, 180)" />
       </Head>
       <Header />
       <main className={styles.main}>
         <div className={styles.grid}>
           <RepoGrid repos={repos} />
         </div>
+        <Script src="https://sean.fish/p/back-arrow-bundle.js" strategy="beforeInteractive"></Script>
       </main>
     </div>
   );
