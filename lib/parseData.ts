7dc314496d0fcc5dc65116df5f881c153393ed26
Sean Breckenridge <seanbrecke@gmail.com>
2023-01-20 15:10:40 -0800

    prefix with image if set

--- lib/parseData.ts
+++ lib/parseData.ts
@@ -75,7 +75,9 @@ async function renderRepo(repo: Repository): Promise<Repository> {
   repo.description = processedDesc.toString();
   if (repo.img != null) {
     repo.dimensions = await sizeOf(path.join(publicDir, repo.img!));
-    repo.img = `${process.env.IMG_PREFIX}${repo.img}`;
+    if (process.env.IMG_PREFIX) {
+      repo.img = `${process.env.IMG_PREFIX}${repo.img}`;
+    }
   }
   return repo;
 }
