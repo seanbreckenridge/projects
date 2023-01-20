94bd9399a9809596e67c6246731711ea1a480e08
Sean Breckenridge <seanbrecke@gmail.com>
2023-01-20 13:13:01 -0800

    fix subdomain

--- components/card.tsx
+++ components/card.tsx
@@ -33,7 +33,7 @@ const RepoCard = React.memo(({ repo }: IRepo) => {
         <div dangerouslySetInnerHTML={{ __html: repo.description }}></div>
         {repo.full_name === "seanbreckenridge/projects" ? (
           <div className={styles.lazyImageContainer}>
-            <iframe src="https://projects.sean.fish" />
+            <iframe src="https://project.sean.fish" />
           </div>
         ) : (
           <LazyImage
