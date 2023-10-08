0b08e9c446dd6d59dc7d8c9299acc62124c75bb0
Sean Breckenridge <seanbrecke@gmail.com>
2023-10-08 00:04:36 -0700

    switch to objectFit contain

--- components/lazy_image.tsx
+++ components/lazy_image.tsx
@@ -14,7 +14,7 @@ const LazyImage = React.memo(({ src, name }: ILazyImage) => {
         <div className={styles.lazyImageContainer}>
           <Image
             style={{
-              objectFit: "cover",
+              objectFit: "contain"
             }}
             src={src}
             alt={name}
